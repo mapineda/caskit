14c09cc07bf8429eceec7b611a7c048f8e3acd75
mapineda <mapineda1222@gmail.com>
2016-08-01 20:00:14 -0500

    created api routing

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,8 @@
 export { App } from './app';
+import * as services from './services';
+
+const mapValueToArray = (obj) => Object.keys(obj).map(key => obj[key]);
+
+export const providers = [
+  ..mapValuesToArray(services)
+];
